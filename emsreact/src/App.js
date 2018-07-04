c995a1f9a34b3c0a97f795d478f7d3add513573e
Emanuele Santanche <esantanche@lily.localhost>
2018-07-04 19:10:34 +0200

    Installed Material UI library

--- emsreact/src/App.js
+++ emsreact/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+// FIXME App.css will have to go
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Button from '@material-ui/core/Button';
+import Index from './pages/index';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. XXXXXX
-        </p>
-      </div>
+        <React.Fragment>
+            <CssBaseline />
+            {/* The rest of your application */}
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+            <Index/>
+
+        </React.Fragment>
     );
   }
 }
