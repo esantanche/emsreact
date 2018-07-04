c1bafb7d0b6504c19ca992ae49048d1a6fd3651a
Emanuele Santanche <esantanche@gmail.com>
2018-07-04 14:31:27 +0000

    Changes on digital ocean server

---
+++ scripts/container-git-tasks.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+if [ `basename ${PWD}` != "scripts" ] ; then
+	echo "You should be in the scripts folder now"
+	exit
+fi
+
+echo "For now it's manual"
+echo "After you run the create react app script, you need a git checkout ."
+echo "which gets rid of the changes create react app did (apart for node_modules)"
+echo "Maybe you need others things"
+echo "probably you need to do some git pull"
+
+exit
+
